4ff90e8d75fc75f528b4cbe08c45a20519fcb11a
Zireael-of-Hope <clarkhustle@yandex.ru>
2024-04-18 00:41:50 +0700

    hangman, wordsDB, interaction classes tests

--- test/test_3_test.dart
+++ test/test_3_test.dart
@@ -1,8 +1,19 @@
-import 'package:test_3/test_3.dart';
+import 'package:dart_application_7/dart_application_7.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('Hangman levels painting class', () {
+    Hangman gallows = Hangman();
+    expect(gallows, isNotNull);
+  });
+
+  test('Words DB class', () {
+    WordsDB wordsDB = WordsDB();
+    expect(wordsDB, isNotNull);
+  });
+
+  test('Interaction class', () {
+    Interaction inetraction = Interaction();
+    expect(inetraction, isNotNull);
   });
 }
