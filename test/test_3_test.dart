a6b996e68e82f4de183a0c3d34215727761e26eb
Zireael-of-Hope <clarkhustle@yandex.ru>
2024-04-18 01:12:31 +0700

    test for topics enum and topics map in wordsDB

--- test/test_3_test.dart
+++ test/test_3_test.dart
@@ -1,5 +1,8 @@
-import 'package:dart_application_7/dart_application_7.dart';
+import 'package:test_3/hangman.dart';
+import 'package:test_3/interaction.dart';
+import 'package:test_3/test_3.dart';
 import 'package:test/test.dart';
+import 'package:test_3/words_db.dart';
 
 void main() {
   test('Hangman levels painting class', () {
@@ -7,9 +10,20 @@ void main() {
     expect(gallows, isNotNull);
   });
 
-  test('Words DB class', () {
-    WordsDB wordsDB = WordsDB();
-    expect(wordsDB, isNotNull);
+  group('words database tests', () {
+    test('Words DB class', () {
+      WordsDB wordsDB = WordsDB();
+      expect(wordsDB, isNotNull);
+    });
+
+    test('enum class topics', () {
+      expect(Topics.countries, isNotNull);
+    });
+
+    test('topics in wordsDB', () {
+      WordsDB wordsDB = WordsDB();
+      expect(wordsDB.topics, isNotNull);
+    });
   });
 
   test('Interaction class', () {
